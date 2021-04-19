352116e21f6f9a48eb81cff296ad2895fc9fb538
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-19 11:38:29 -0300

    added drawer cart component to display the cart

--- src/views/MenuQr.jsx
+++ src/views/MenuQr.jsx
@@ -1,27 +1,56 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Products from '../components/Products';
+import DrawerCart from '../components/DrawerCart'
 import productService from '../services/products-service';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon, Tooltip, Button, Box, Stack } from '@chakra-ui/react';
+import { editCart } from '../utils/editCart';
+import { ArrowLeftIcon } from '@chakra-ui/icons'
 
 const MenuQr = () => {
 
     const [loading, setLoading] = useState(false);
     const [products, setProducts] = useState([]);
+    const [isDrawerOpen, setDrawerOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
 
-    useEffect(() => {    
+    useEffect(() => {
         const fetchData = async () => {
-          setLoading(true);
-          const products = await productService.allProducts();
-          setProducts(products)
-          setLoading(false);
+            setLoading(true);
+            const products = await productService.allProducts();
+            setProducts(products)
+            setLoading(false);
         }
 
         fetchData();
-      }, []);
+    }, []);
 
-    return(
+    const handleEditCart = (product, action) => {
+        setCartItems(editCart(product, action));
+    }
+    return (
         <Flex justifyContent={"center"} height="100vh">
-            {loading ? <p> Cargando... </p> :<Products products = {products} loading = {loading} /> }
+            {loading ? <p> Cargando... </p> :
+                <Stack>
+                    <Flex justifyContent="flex-end" position="fixed" right="2%">
+                        <Button bg="theme.200" color="theme.100" onClick = {() => setDrawerOpen(true)}>
+                            +
+                        </Button>                        
+                    </Flex>
+                    <Flex>
+                        <Products
+                            products={products}
+                            loading={loading}
+                            onAddProduct={(product) => handleEditCart(product, "add")}
+                        />
+                        <DrawerCart
+                            items={cartItems}
+                            onClose={() => setDrawerOpen(false)}
+                            isOpen={isDrawerOpen}
+                            onDeleteProduct={(product) => handleEditCart(product, "delete")}
+                        />
+                    </Flex>
+                </Stack>
+            }
         </Flex>
     );
 
