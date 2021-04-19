987f3a23aa0838c1aa8bf2b296ce3ef035b2a4b7
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-19 11:37:43 -0300

    fix some css issues

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,26 @@
-import { Stack, Button, Text, Image, Heading } from "@chakra-ui/react"
+import { Stack, Text, Image } from "@chakra-ui/react"
 import {parseCurrency} from "../utils/currency";
 
-const Product = (product) => {
-    const {id, name, description, price, img} = product.product;
+const Product = ({product, onAddProduct}) => {
+    const {id, name, description, price, img} = product;
 
     return (
         <Stack
             key={id}
-            borderColor="gray.400"
+            borderColor="theme.300"
             borderRadius="md"
-            borderWidth={3}
             justifyContent="space-between"
             spacing={1}
             marginBottom={3}
             sm="30em"
             shadow="md"
             backgroundAttachment={img}
-            onClick= {() => console.log("agregar...")}
+            onClick= {() => onAddProduct(product)}
             as="button"
+            alignItems={"center"} 
         >            
-            <Stack direction="column" padding={0} >
-                <Text fontSize="50px" color="gray.600">{name}</Text>
+            <Stack  direction="column" padding={0} >
+                <Text fontSize="50px" color="theme.100">{name}</Text>
                 <Image
                     src={img}
                     backgroundColor="white"
@@ -30,10 +30,10 @@ const Product = (product) => {
                 />
                 <Stack justifyContent="space-between" spacing={1}>                    
                     <Stack spacing={1}>                        
-                        <Text color="blue.500" fontSize="sm" >{description}</Text>
+                        <Text color="theme.200" fontSize="xl" >{description}</Text>
                     </Stack>
                     <Stack justifyContent="space-between">
-                        <Text color="red.700" fontSize="sm" fontWeight="700"> {parseCurrency(price)} </Text>
+                        <Text color="theme.500" fontSize="sm" fontWeight="700"> {parseCurrency(price)} </Text>
                     </Stack>
                 </Stack>
             </Stack>
