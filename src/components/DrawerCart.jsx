a248de338989c22066d3f11fc23197ca99c667c3
PaisMariano <mariano_a_p@hotmail.com>
2021-04-22 00:12:01 -0300

    added order confirmation with alert

--- src/components/DrawerCart.jsx
+++ src/components/DrawerCart.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, Button, Text } from '@chakra-ui/react';
+import { Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, Button, Text, Alert, AlertIcon } from '@chakra-ui/react';
 import { Flex } from "@chakra-ui/layout";
+import OrderService from '../services/orders-service';
 import Items from './Items';
 import { parseCurrency } from "../utils/currency";
 
 
 const DrawerCart = ({ items, onClose, isOpen, onDeleteProduct, tableId, ...props }) => {
-
     const [total, setTotal] = useState(0)
-
+    //Order confirmation
+    const [isAdded, setIsAdded] = useState(false);
+    const confirmationAlert = () => { setTimeout(() => setIsAdded(false), 2000); }
+    
     useEffect(() => {
         const calculate = () => {
             const red = items.reduce((total, item) => total + item.product.price * item.amount, 0 );
@@ -24,6 +27,19 @@ const DrawerCart = ({ items, onClose, isOpen, onDeleteProduct, tableId, ...props
             tableId:tableId,
             orderedItems:items
         }
+        OrderService.confirmOrder(orderDTO);
+        setIsAdded(true);    
+    }
+
+    function renderOrderConfirmationCheck() {
+        confirmationAlert();
+        setTimeout(() => onClose(), 2000)
+        return (
+            <Alert status="success" variant="solid" justifyContent="center" textAlign="center">
+                <AlertIcon />
+                El pedido fue confirmado!
+            </Alert>
+        )
     }
 
     return (
@@ -72,6 +88,7 @@ const DrawerCart = ({ items, onClose, isOpen, onDeleteProduct, tableId, ...props
                             <Button mr={2} bg="gray.100" color="theme.100" variant="outline" onClick={onClose}>Cancel</Button>
                             <Button onClick= {() => dispatchCreateCart()} bg="theme.300" color="theme.100">Confirmar</Button>
                         </DrawerFooter>
+                        {isAdded ? renderOrderConfirmationCheck() : null}
                     </DrawerContent>
                 </DrawerOverlay>
             </Drawer>
