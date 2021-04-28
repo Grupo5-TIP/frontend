95e667e78f558ccc70e4f79bbcd4f0752ec0f9f9
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-28 18:37:12 -0300

    basic template to display tables on small devices

--- src/services/tables-service.js
+++ src/services/tables-service.js
@@ -4,12 +4,15 @@ import { REST_API_URL } from './constants'
 const API_URL = `${REST_API_URL}/tables`;
 
 class TablesService{
-    async getAllProducts(){
+    async getTables(){
         //return axios.get(API_URL);
         const tables =  [
             {id:1,  x:10, y:10, state:"inUse"},
             {id:2,  x:30, y:10, state:"empty"},
             {id:3,  x:50, y:10, state:"inUse"},
+            {id:4,  x:50, y:10, state:"empty"},
+            {id:5,  x:50, y:10, state:"empty"},
+            {id:6,  x:50, y:10, state:"empty"},
         ];
         return tables;
         
--- src/views/Tables.jsx
+++ src/views/Tables.jsx
@@ -1,16 +1,46 @@
+import { useState, useEffect } from 'react'
 import { Flex } from "@chakra-ui/layout";
+import { Text } from '@chakra-ui/react';
 import Table from '../components/Table';
+import tablesService from '../services/tables-service';
 
-const Tables = ({tables}) => {
+const Tables = () => {
+    const [tables, setTables] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            await tablesService.getTables()
+                .then(resp => {
+                    setTables(resp);
+                })
+                .catch(err => {
+                    setError(err);
+                });
+            setLoading(false);
+        }
+
+        fetchData();
+    }, []);
+    
+    
     return (
-        <Flex flexDirection={"column"}> 
-            {tables.map( table =>
-                <Table
-                    key = {table.id}
-                    table={table}
-                />
-                )
-           }
+        <Flex flexDirection={"column"} minHeight="80vh">
+
+            { error !== '' ? <Text color="gray.400">Error al traer mesas del server...</Text>
+                :
+                loading ? <Text color="gray.400"> Cargando... </Text>
+                    :
+                    tables.map(table =>
+                        <Table
+                            key={table.id}
+                            table={table}
+                        />
+                    )
+            }
         </Flex>
     )
 }
