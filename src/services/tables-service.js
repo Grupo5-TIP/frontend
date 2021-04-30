2dd91f0287a7dcb714e49596a22c07a74ca4e227
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-30 01:03:03 -0300

    added desktop view and fix some mobile property in tables view

--- src/services/tables-service.js
+++ src/services/tables-service.js
@@ -7,12 +7,12 @@ class TablesService{
     async getTables(){
         //return axios.get(API_URL);
         const tables =  [
-            {id:1,  x:10, y:10, state:"inUse"},
-            {id:2,  x:30, y:10, state:"empty"},
-            {id:3,  x:50, y:10, state:"inUse"},
-            {id:4,  x:50, y:10, state:"empty"},
-            {id:5,  x:50, y:10, state:"empty"},
-            {id:6,  x:50, y:10, state:"empty"},
+            {id:1,  x:0, y:10, state:"inUse", size:6},
+            {id:2,  x:450, y:10, state:"empty", size:4},
+            {id:3,  x:900, y:100, state:"inUse", size:8},
+            {id:4,  x:0, y:330, state:"empty", size:2},
+            {id:5,  x:450, y:500, state:"empty", size:2},
+            {id:6,  x:900, y:300, state:"empty", size:2},
         ];
         return tables;
         
