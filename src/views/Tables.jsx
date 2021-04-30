ed7accb294e1ebf670f34045aea043eefda7a2be
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-04-30 01:03:03 -0300

    added desktop view and fix some mobile property in tables view

--- src/views/Tables.jsx
+++ src/views/Tables.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Flex } from "@chakra-ui/layout";
-import { Text } from '@chakra-ui/react';
+import { Text, useMediaQuery } from '@chakra-ui/react';
 import Table from '../components/Table';
 import tablesService from '../services/tables-service';
 
@@ -8,7 +8,7 @@ const Tables = () => {
     const [tables, setTables] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
-
+    const [isLarger] = useMediaQuery("(min-width: 380px)");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,10 +25,53 @@ const Tables = () => {
 
         fetchData();
     }, []);
-    
-    
+
     return (
-        <Flex flexDirection={"column"} minHeight="80vh">
+        <Flex>
+            {
+                isLarger ?
+                    <Flex
+                        flexWrap="wrap"
+                        justifyContent="center"
+                        minHeight="90vh"
+                    >
+                        {error !== '' ? <Text color="gray.400">Error al traer mesas del server...</Text>
+                            :
+                            loading ? <Text color="gray.400"> Cargando... </Text>
+                                :
+                                tables.map(table => 
+                                    <Table
+                                        key={table.id}
+                                        table={table}
+                                    />
+                                )
+                        }
+                    </Flex>
+                    :
+                    <Flex
+                        flexDirection={"column"}
+                    >
+                        {error !== '' ? <Text color="gray.400">Error al traer mesas del server...</Text>
+                            :
+                            loading ? <Text color="gray.400"> Cargando... </Text>
+                                :
+                                tables.map(table =>
+                                    <Table
+                                        key={table.id}
+                                        table={table}
+                                    />
+                                )
+                        }
+                    </Flex>
+            }
+        </Flex>
+
+        /*<Flex   
+            //flexWrap="wrap" // delete when we use the xy values
+            //justifyContent="center"
+            //minHeight="90vh"
+            flexDirection={"column"}
+            >
 
             { error !== '' ? <Text color="gray.400">Error al traer mesas del server...</Text>
                 :
@@ -41,7 +84,7 @@ const Tables = () => {
                         />
                     )
             }
-        </Flex>
+        </Flex>*/
     )
 }
 
