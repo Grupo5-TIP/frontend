dbc1a7f7163bac28a6b44e200c0e9bce1de18082
Pablo Marrero <pablo.g.marrero@gmail.com>
2021-06-13 12:05:13 -0300

    Feature invoice option (#20)
    * first approach still needs to be better
    
    * Delete amount payment on Invoice DialogDisplay

--- src/components/DialogDisplay.jsx
+++ src/components/DialogDisplay.jsx
@@ -1,9 +1,13 @@
 import React from "react"
-import { AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogBody,
-    AlertDialogHeader, AlertDialogFooter , AlertDialogCloseButton, Button } from '@chakra-ui/react';
+import {
+    AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogBody,
+    AlertDialogHeader, AlertDialogFooter, AlertDialogCloseButton, Button,
+    FormControl, FormLabel, Radio, RadioGroup, HStack, Flex, Input, Box
+} from '@chakra-ui/react';
 import { hover } from '../utils/buttonDesign';
+import { useState } from "react";
 
-const DialogDisplay = ({ header, firstOption, secondOption, message, action, isOpen, onClose, onCloseAll }) => {
+const DialogDisplay = ({ header, firstOption, secondOption, message, action, isOpen, onClose, onCloseAll, body, payment }) => {
     const cancelRef = React.useRef();
 
     const onClick = () => {
@@ -11,6 +15,22 @@ const DialogDisplay = ({ header, firstOption, secondOption, message, action, isO
         onCloseAll();
     }
 
+
+    const FieldsOnInvoice = () => {
+        return (
+            <Flex padding={3}>
+                <FormControl as="fieldset">
+                    <FormLabel as="invoice-option">Método de pago elegido</FormLabel>
+                    <RadioGroup defaultValue="EF">
+                        <HStack spacing="24px">
+                            <Radio value="EF" id="valueEF" onChange={()=> payment.method = "EF"}>Efectivo</Radio>
+                            <Radio value="MP" id="valueMP" onChange={()=> payment.method = "MP"}>Mercado pago</Radio>
+                        </HStack>
+                    </RadioGroup>
+                </FormControl>
+            </Flex>
+        )
+    }
     return (
         <AlertDialog
             leastDestructiveRef={cancelRef}
@@ -24,24 +44,30 @@ const DialogDisplay = ({ header, firstOption, secondOption, message, action, isO
             <AlertDialogOverlay />
 
             <AlertDialogContent>
-            <AlertDialogHeader>{header}</AlertDialogHeader>
-            <AlertDialogCloseButton />
-            <AlertDialogBody>
-                {message}
-            </AlertDialogBody>
-            <AlertDialogFooter>
-                <Button bg="gray.100" color="theme.100" variant="outline" ref={cancelRef} onClick={onClose}>
-                    {firstOption}
-                </Button>
-                <Button bg="theme.100" 
-                    color="white" 
-                    margin="3px" 
-                    _hover={hover} 
-                    ml={3} 
-                    onClick={onClick}>
-                    {secondOption}
-                </Button>
-            </AlertDialogFooter>
+                <AlertDialogHeader>{header}</AlertDialogHeader>
+                <AlertDialogCloseButton />
+                <AlertDialogBody>
+                    {message}
+                    {body ?
+                        <FieldsOnInvoice />
+                        :
+                        null
+                    }
+                </AlertDialogBody>
+                <AlertDialogFooter>
+                    <Button bg="gray.100" color="theme.100" variant="outline" ref={cancelRef} onClick={onClose}>
+                        {firstOption}
+                    </Button>
+                    <Button bg="theme.100"
+                        color="white"
+                        margin="3px"
+                        _hover={hover}
+                        ml={3}
+                        onClick={onClick}
+                    >
+                        {secondOption}
+                    </Button>
+                </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
     )
