3db36a1ae28b6932ba355696a23e1ca0316e4b0f
PabloGMarrero <pablo.g.marrero@gmail.com>
2021-07-05 21:43:52 -0300

    first approach on feature dashboard

---
+++ src/components/VerticalChart.jsx
@@ -0,0 +1,81 @@
+import { Bar } from 'react-chartjs-2';
+import { useState, useEffect } from 'react'
+import { Flex, Box } from '@chakra-ui/react'
+import kpiService from '../services/kpi-service';
+import StatusAlertDisplay from '../components/StatusAlertDisplay';
+import Loading from '../components/Loading';
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+    },
+};
+
+const VerticalBar = ({dataTemplate}) => {
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [dataToDisplay, setDataToDisplay] = useState({});
+    const dataMonths = [];
+    const dataAmounts = [];
+
+    useEffect(() => {
+        const fetchData = async () => {
+            kpiService.getDataLastMonths()
+                .then(resp => {
+                    handleData(resp)
+                }).catch(err => {
+                    console.log(err);
+                    setError(err)
+                });
+        }
+
+        const handleData = (resp) => {
+            resp.map(month => {
+                dataMonths.push(month.name);
+                dataAmounts.push(month.amount);
+            })
+            dataTemplate.labels = dataMonths;
+            dataTemplate.datasets[0].data = dataAmounts;
+            setDataToDisplay(dataTemplate);
+            setLoading(false);
+        }
+
+        setLoading(true);
+        fetchData();
+    }, [dataTemplate]);
+
+
+
+    if (error !== '') {
+        return (
+            <StatusAlertDisplay top={2}
+                padding={5}
+                margin="0 auto"
+                h="150px"
+                w="500px"
+                boxShadow="lg"
+                status="error"
+                message="Error al traer del server..."
+            />
+        )
+    }
+    return (
+        <Flex>
+            {
+                loading ? <Box width="100%"><Loading /></Box> :
+                    <Bar width={600} height={600} data={dataToDisplay} options={options} />
+            }
+
+        </Flex>
+
+
+    )
+};
+
+export default VerticalBar;
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,12 +1,62 @@
-import { withRouter, Redirect  } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react'
+import { withRouter, Redirect } from 'react-router-dom';
+import { Flex, Stack, Heading } from '@chakra-ui/react'
 import { validateLogin } from '../utils/validate-login';
+import VerticalBar from '../components/VerticalChart';
+import PieChart from '../components/PieChart';
+
+const dataTemplate = {
+    labels: [],
+    datasets: [
+        {
+            label: 'Venta en $ARS',
+            data: [],
+            backgroundColor: [
+                'rgba(251, 139, 36)',
+                'rgba(217, 3, 104)',
+                'rgba(130, 2, 99)',
+                'rgba(41, 23, 32)',
+                'rgba(4, 167, 119)',
+                'rgba(3, 3, 1)',
+                'rgba(255, 67, 101)',
+                'rgba(0, 217, 192)',
+                'rgba(136, 209, 138)',
+                'rgba(200, 62, 77)',
+                'rgba(50, 55, 59)',
+            ],
+            borderColor: [
+                'rgba(251, 139, 36)',
+                'rgba(217, 3, 104)',
+                'rgba(130, 2, 99)',
+                'rgba(41, 23, 32)',
+                'rgba(4, 167, 119)',
+                'rgba(3, 3, 1)',
+                'rgba(255, 67, 101)',
+                'rgba(0, 217, 192)',
+                'rgba(136, 209, 138)',
+                'rgba(200, 62, 77)',
+                'rgba(50, 55, 59)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
+
 
 const Checkout = (...props) => {
-    return(
-        <Flex justifyContent={"center"} flexGrow={1} width="100%">
+
+
+    return (
+        <Flex margin={10} flexGrow={1}>
             {
-                !validateLogin() ? <Redirect to="/" /> : <p>Dashboard...</p>
+                !validateLogin() ? <Redirect to="/" />
+                    :
+                    <Stack>
+                        <Heading as="h2" size="3xl" paddingTop={0} fontSize="md" data-testid="" fontWeight={600}> Acumulado del año </Heading>
+                        <Stack direction="row" padding={5}>
+                            <VerticalBar dataTemplate={dataTemplate} />
+                            <PieChart dataTemplate={dataTemplate}/>
+                        </Stack>
+                    </Stack>
             }
         </Flex>
     )
